7d1659a661ea1babb016a82534087539a46afc6c
ead <ead@academico.local>
2024-08-13 17:40:00 -0300

    dia13/08

---
+++ alomundotarde/classes/timefut.py
@@ -0,0 +1,6 @@
+# oque e uma
+class Timefut():
+    def __init__(self, nome,vitoria):
+        self.nome = nome
+        self.vitorias = vitorias
+        
--- alomundotarde/inicio.py
+++ alomundotarde/inicio.py
@@ -1,11 +1,16 @@
-print("alo mundo")
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/inicio")
-def ola():
-    return "ola mundo"
-
-@app.route("/olamundo")
-def mostrar():
-    return render_template ('pagina.html')
-app.run()
+class Matematica():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def somar(self):
+        return self.x + self.y
+    def subtrair(self):
+        return self.x - self.y
+    def multiplicar(self):
+        return self.x * self.y
+    def dividir(self):
+        return self.x / self.y
+    def potencia(self):
+        return self.x ** self.y
+    def raiz(self):
+        return self.x ** (1/self.y)
